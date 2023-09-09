16b8dac9a0d3b824beeacc03f69c5b26bf72b2af
VarunPruthi <varunpruthi1682001@gmail.com>
2023-09-09 22:09:40 +0530

    create a new file name as bleg.js

---
+++ bleg.js
@@ -0,0 +1,7 @@
+
+Sunday
+3 Oct 2020
+HACKERS WORLD
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos ipsam, nihil, quod eos facilis iure inventore nobis reiciendis adipisci eveniet ullam voluptatibus incidunt voluptates cum autem voluptatum expedita ea? Ipsum.
+
+READ MORE
